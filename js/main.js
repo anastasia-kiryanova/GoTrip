bb00a4e43d17c54ee2b4c6e62ad582f90ae2c93d
Anastasia_Kiryanova <a.kiryanova99@yandex.ru>
2022-09-14 17:10:50 +0500

    mobile version

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
+//Slider
 const owl = $('.owl-carousel');
 
 owl.owlCarousel({
   center: true,
   loop: true,
-  margin: 30,
+  margin: 20,
   startPosition: 1,
-  items: 3,
+  items: 1,
+  responsive : {
+    500 : {
+      items: 3,
+    },
+    1200 : {
+      margin: 30,
+      items: 3,
+    },
+  }
 });
 
 $('.slider__btn--prev').click(function() {
@@ -15,3 +25,15 @@ $('.slider__btn--prev').click(function() {
 $('.slider__btn--next').click(function() {
     owl.trigger('next.owl.carousel');
 });
+
+
+//Menu Icon
+const navButton = document.querySelector('.nav__toggle');
+const nav = document.querySelector('.nav');
+const menuIcon = document.querySelector('.menu-icon');
+
+navButton.onclick = function(){
+    nav.classList.toggle('nav--mobile');
+    menuIcon.classList.toggle('menu-icon-active');
+    document.body.classList.toggle('no-scroll')
+}
